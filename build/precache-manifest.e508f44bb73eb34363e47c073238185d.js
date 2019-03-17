0aaec81f903125570c8ec34d604f3eaac6f7beb5
Priscilla Lo <priscillalo@cmail.carleton.ca>
2019-03-17 17:29:07 -0400

    Initial working commit

---
+++ build/precache-manifest.e508f44bb73eb34363e47c073238185d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24f81a7361ccaf76129e",
+    "url": "/static/js/main.c413effa.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "24f81a7361ccaf76129e",
+    "url": "/static/css/main.61e1f92a.chunk.css"
+  },
+  {
+    "revision": "17128ba3f00c0e98e7b728064cdebd6e",
+    "url": "/index.html"
+  }
+];
