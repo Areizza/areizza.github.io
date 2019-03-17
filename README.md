0a602fb4e1d131e8f75d5be06a2d27200727fe17
Priscilla Lo <priscillalo@cmail.carleton.ca>
2019-03-17 18:16:36 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-﻿This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-Link: https://areizza.github.io/
---
+++ precache-manifest.6cbe06435a65f4ed65c7fa1d7a5884b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/testReactApp/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "8c2503be183f82fae2f2",
+    "url": "/testReactApp/static/js/runtime~main.be815000.js"
+  },
+  {
+    "revision": "9b9fbf462bfff731f28c",
+    "url": "/testReactApp/static/js/main.c077f3e4.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/testReactApp/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "9b9fbf462bfff731f28c",
+    "url": "/testReactApp/static/css/main.5cf5893c.chunk.css"
+  },
+  {
+    "revision": "91da504bd055dd7f2a0f43cc7f47e24b",
+    "url": "/testReactApp/index.html"
+  }
+];
